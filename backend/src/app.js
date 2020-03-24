7233039a4f2c197a4994008ff619a35e8fb09d62
carloshrf <carlos_6282010@hotmail.com>
2020-03-24 15:05:53 -0300

    API Backend completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 class App {
   constructor() {
     this.server = express();
 
-    this.routes();
+    this.server.use(cors());
     this.middlewares();
+    this.routes();
   }
 
   middlewares() {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,21 @@
+const OngController = require('./app/controllers/OngController');
+const IncidentController = require('./app/controllers/IncidentController');
+const ProfileController = require('./app/controllers/ProfileController');
+const SessionController = require('./app/controllers/SessionController');
+
 const { Router } = require('express');
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ok: true});
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
